733edf48fa10a17ff67d07ac0ef1aaab5ae6b000
junaid1506 <junnu2757@gmail.com>
2025-10-10 18:17:07 +0530

    Update

--- Projects/todo.js
+++ Projects/todo.js
@@ -7,5 +7,16 @@ function addTodo(){
     let InputElement = document.querySelector('#todoItem')
     let todoItem = InputElement.value
     todoList.push(todoItem)
-    
+    InputElement.value = ''
+
+    for (let i = 0; i < todoList.length; i++) {
+      console.log(todoList[i])
+                                                                                
+    }
+
+
+
+
+
 }
+
