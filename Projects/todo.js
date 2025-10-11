c2481d4477c9a7bc4b797bc5f2c846d639f2b224
junaid1506 <junnu2757@gmail.com>
2025-10-11 13:58:30 +0530

    Update

--- Projects/todo.js
+++ Projects/todo.js
@@ -1,30 +1,45 @@
-let todoList = []
-displayTodo()
+let todoList = [
+
+  {
+    item : "Doodh lene jana hai",
+    dueDate : "12/10/2025"
+  },
+  {
+    item : "Assisment sumbit krne hai",
+    dueDate : "12/10/2025"
+  }
 
 
 
-function addTodo(){
-    let InputElement = document.querySelector('#todoItem')
-    let todoItem = InputElement.value
-    todoList.push(todoItem)
-    InputElement.value = ''
-    displayTodo()
+];
+displayTodo();
+
+function addTodo() {
+  let InputElement = document.querySelector("#todoItem");
+  let InputDate = document.querySelector("#todoDate");
+  let todoItem = InputElement.value;
+  let todoDate = InputDate.value;
+  todoList.push({
+    item:todoItem,
+    todoDate:todoDate
+  });
+  InputElement.value = "";
+  InputDate.value = ""
+  displayTodo();
 }
 
-
-function displayTodo(){
-    let todos = document.querySelector('#todos');
-    let newHtml = '';
-    for (let i = 0; i < todoList.length; i++) {
-        newHtml += `
+function displayTodo() {
+  let todos = document.querySelector("#todos");
+  let newHtml = "";
+  for (let i = 0; i < todoList.length; i++) {
+    newHtml += `
         <div>
           <span>${todoList[i]}</span>
-          <button>Delete</button>
+          <button onclick="todoList.splice(${i}, 1) 
+          displayTodo()">Delete</button>
         </div>  
-      `; 
-    }
-    console.log(newHtml)
-   todos.innerHTML = newHtml;
+      `;
+  }
+  console.log(newHtml);
+  todos.innerHTML = newHtml;
 }
-
-
