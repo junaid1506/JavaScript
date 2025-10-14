6bd5d8a17e2514492a55d90d982582879ce3ec76
junaid1506 <junnu2757@gmail.com>
2025-10-14 17:05:25 +0530

    Update

--- Projects/Myntra clone/scripts/index.js
+++ Projects/Myntra clone/scripts/index.js
@@ -5,19 +5,19 @@ items.forEach((item)=>{
 
     innerHTML += `
     <div class="item-container">
-        <img class="item-img" src="${item.image}" alt="${item.item_name}">
-        <div class="rating">
-            ${item.rating.stars}⭐ | ${item.rating.count}
-        </div>
-        <div class="company-name">${item.company}</div>
-        <div class="item-name">${item.item_name}</div>
-        <div class="price">
-            <span class="current-price">Rs ${item.current_price}</span>
-            <span class="original-price">Rs ${item.original_price}</span>
-            <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
-        </div>
-        <button class="btn-add-bag">Add To Bag</button>
-    </div>
+                <img class="item-img" src='${item.image}'alt="item1">
+                <div class="rating">
+                    ${item.rating.stars}⭐ | ${item.rating.count} 
+                </div>
+                <div class="company-name">${item.company}</div>
+                <div class="item-nam">${item.item_name}</div>
+                <div class="pric">
+                    <span class="currnet-price">Rs ${item.current_price}</span>
+                    <span class="orignal-price">Rs ${item.original_price} </span>
+                    <span class="discount-price">(${item. discount_percentage}% OFF)</span>
+                </div>
+                <button class="btn-add-bag">Add To Bag</button>
+            </div>
   `;
 })
 
