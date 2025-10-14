2b780591c6433ba09db5c1f4c6f1655906ecabc5
junaid1506 <junnu2757@gmail.com>
2025-10-14 17:09:54 +0530

    Update

--- Projects/Myntra clone/scripts/index.js
+++ Projects/Myntra clone/scripts/index.js
@@ -23,19 +23,5 @@ items.forEach((item)=>{
 
  itemsContainerElement.innerHTML = innerHTML
 
-//   <div class="item-container">
-//                 <img class="item-img" src='${items.item_image}'alt="item1">
-//                 <div class="rating">
-//                     ${items.ratings.stars}⭐ | ${items.ratings.numOfReview} 
-//                 </div>
-//                 <div class="company-name">${items.company_name}</div>
-//                 <div class="item-nam">${items.item_name}</div>
-//                 <div class="pric">
-//                     <span class="currnet-price">Rs ${items.current_price}</span>
-//                     <span class="orignal-price">Rs ${items.orignal_price} </span>
-//                     <span class="discount-price">(${items.discount_price}% OFF)</span>
-//                 </div>
-//                 <button class="btn-add-bag">Add To Bag</button>
-//             </div>
 
-// `;
+ 
