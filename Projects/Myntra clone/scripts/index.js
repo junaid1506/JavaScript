ff30c9e2164f86d261c04f30d5860962073b56bf
junaid1506 <junnu2757@gmail.com>
2025-10-14 16:20:24 +0530

    Update

---
+++ Projects/Myntra clone/scripts/index.js
@@ -0,0 +1,41 @@
+let itemsContainerElement = document.querySelector(".items-container");
+let innerHTML = '';
+
+items.forEach((item)=>{
+
+    innerHTML += `
+    <div class="item-container">
+        <img class="item-img" src="${item.image}" alt="${item.item_name}">
+        <div class="rating">
+            ${item.rating.stars}⭐ | ${item.rating.count}
+        </div>
+        <div class="company-name">${item.company}</div>
+        <div class="item-name">${item.item_name}</div>
+        <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+        </div>
+        <button class="btn-add-bag">Add To Bag</button>
+    </div>
+  `;
+})
+
+ itemsContainerElement.innerHTML = innerHTML
+
+//   <div class="item-container">
+//                 <img class="item-img" src='${items.item_image}'alt="item1">
+//                 <div class="rating">
+//                     ${items.ratings.stars}⭐ | ${items.ratings.numOfReview} 
+//                 </div>
+//                 <div class="company-name">${items.company_name}</div>
+//                 <div class="item-nam">${items.item_name}</div>
+//                 <div class="pric">
+//                     <span class="currnet-price">Rs ${items.current_price}</span>
+//                     <span class="orignal-price">Rs ${items.orignal_price} </span>
+//                     <span class="discount-price">(${items.discount_price}% OFF)</span>
+//                 </div>
+//                 <button class="btn-add-bag">Add To Bag</button>
+//             </div>
+
+// `;
